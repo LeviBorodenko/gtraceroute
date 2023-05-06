02b74dc662b093440aa97164e29eaf90698dcb70
Levi Borodenko <levi.borodenko@smart-steel-technologies.com>
2023-05-06 12:19:42 +0200

    wip

--- socket_test.py
+++ socket_test.py
@@ -2,8 +2,10 @@ import enum
 import socket
 import time
 import struct
+import asyncio
 from random import randbytes
 from dataclasses import dataclass, field
+from typing import AsyncGenerator
 
 PROBE_BASE_PORT = 12345
 PROBE_UDP_PAYLOAD_SIZE = 8
@@ -62,8 +64,8 @@ class InvalidProbeReplyException(Exception):
 
 
 class ProbeStatus(enum.Enum):
-    PENDING = 0
-    IN_PROGRESS = 1
+    NOT_DISPATCHED = 0
+    DISPATCHED = 1
     SUCCESSFUL = 2
     FAILED = 3
 
@@ -71,22 +73,36 @@ class ProbeStatus(enum.Enum):
 @dataclass
 class ProbeRequest:
     ipv4: str
+
+    @property
+    def ipv4_bytes(self) -> bytes:
+        return socket.inet_pton(socket.AF_INET, self.ipv4)
+
     ttl: int
     port: int = PROBE_BASE_PORT
     udp_payload: bytes = field(
         default_factory=lambda: randbytes(PROBE_UDP_PAYLOAD_SIZE)
     )
-    request_creation_ts: float = field(default_factory=lambda: time.time())
-    status: ProbeStatus = ProbeStatus.PENDING
-    retry_count: int = 0
+    probe_request_creation_ts: float = field(default_factory=lambda: time.time())
+    status: ProbeStatus = ProbeStatus.NOT_DISPATCHED
     probe_dispatch_ts: float | None = None
 
     @staticmethod
-    def generate_ttl_sweep(target_ipv4: str, max_ttl: int = 16) -> list["ProbeRequest"]:
+    def generate_ttl_sweep(host: str, max_ttl: int = 16) -> list["ProbeRequest"]:
         sweep = []
         for i in range(1, max_ttl + 1):
-            sweep.append(ProbeRequest(target_ipv4, i))
+            sweep.append(ProbeRequest(get_ipv4(host), i))
         return sweep
+
+
+class RouteHop:
+    target_ipv4: str
+    hop: int
+    estimated_rtt: float | None = None
+
+    def __init__(self, target_ipv4: str, hop: int) -> None:
+        self.target_ipv4 = target_ipv4
+        self.hop = hop
 
 
 @dataclass
@@ -134,76 +150,87 @@ class ProbeReply:
         )
 
 
-def main():
-    target_ipv4 = get_ipv4("google.com")
-    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+async def async_recv(sock: socket.socket) -> bytes:
+    data = await asyncio.get_event_loop().sock_recv(sock, 1024)
+    return data
 
-    ICMP_PROTO = socket.getprotobyname("icmp")
-    icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, ICMP_PROTO)
-    icmp_socket.settimeout(5)
 
-    sweep = ProbeRequest.generate_ttl_sweep(target_ipv4, 12)
+async def async_sendto(sock, udp_payload: bytes, addr: tuple[str, int]):
+    await asyncio.get_event_loop().sock_sendto(sock, udp_payload, addr)
 
+
+class ICMPReplyWatcher:
+    icmp_socket: socket.socket
+    dispatched_probe_requests: list[ProbeRequest] = []
+
+    def __init__(self, timeout: int = 5) -> None:
+        icmp_socket = socket.socket(
+            socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname("icmp")
+        )
+        icmp_socket.setblocking(False)
+        icmp_socket.settimeout(timeout)
+        self.icmp_socket = icmp_socket
+
+    async def probe_reply_stream(self) -> AsyncGenerator[ProbeReply, None]:
+        while True:
+            packet_bytes = await async_recv(self.icmp_socket)
+            yield ProbeReply.from_bytes(packet_bytes)
+
+
+class ProbeDispatcher:
+    udp_socket: socket.socket
+
+    def __init__(self) -> None:
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_socket.setblocking(False)
+        self.udp_socket = udp_socket
+
+    async def dispatch(self, probe: ProbeRequest):
+        self.udp_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, probe.ttl)
+        await async_sendto(self.udp_socket, probe.udp_payload, (probe.ipv4, probe.port))
+
+
+async def async_main():
+
+    probe_dispatcher = ProbeDispatcher()
+    reply_watcher = ICMPReplyWatcher(5)
+    sweep = ProbeRequest.generate_ttl_sweep("kkos.net")
     for probe in sweep:
-        udp_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, probe.ttl)
-        time_bs = time.time()
-        udp_socket.sendto(probe.udp_payload, (probe.ipv4, probe.port))
-        probe.status = ProbeStatus.IN_PROGRESS
-        probe.probe_dispatch_ts = time_bs
+        await probe_dispatcher.dispatch(probe)
 
-    while True:
-        try:
-            data = icmp_socket.recv(1024)
-            probe_reply = ProbeReply.from_bytes(data)
-            print(probe_reply)
-            print("_______________________")
-        except socket.timeout:
-            print("No ICMP packet arrived before timeout. Stopping sweep.")
-            break
-        except InvalidProbeReplyException as e:
-            raise e
+    async for reply in reply_watcher.probe_reply_stream():
+        print(reply)
 
-    # rtt = time.time() - time_bs
-    # ipv4_header = IPv4Header.from_bytes(data[:20])
-    # print(
-    #     f"[Hops: {hops}, {rtt:.2f}ms] Received IPv4 package in ICMP socket: {ipv4_header}..."
-    # )
-    # if ipv4_header.source_ip == target_ipv4:
-    #     print("ICMP comes from the target!")
-    #     break
-    #
-    # assert ipv4_header.protocol == 1
-    #
-    # icmp_header = ICMPHeader.from_bytes(data[20:24])
-    # print(f"... it contains an ICMP message: {icmp_header}")
-    #
-    # icmp_ref_ipv4_header = IPv4Header.from_bytes(data[28:48])
-    # print(f"... which references an IPv4 package: {icmp_ref_ipv4_header}")
-    # if icmp_ref_ipv4_header.dst_ip != target_ipv4:
-    #     print(f"Referenced IPv4 package dst_ip does not match!")
-    #     continue
-    # if icmp_ref_ipv4_header.ttl != 1:
-    #     print(f"Referenced IPv4 package has TTL={icmp_ref_ipv4_header.ttl}")
-    #
-    # icmp_ref_udp_header = UDPHeader.from_bytes(data[48:56])
-    # print(f"... which contains a UDP package: {icmp_ref_udp_header}")
-    # if icmp_ref_udp_header.dst_port != port:
-    #     print("Referenced UDP destination port does not match!")
-    #     continue
-    #
-    # recv_udp_payload = data[56:64]
-    # if len(recv_udp_payload) == 0:
-    #     print("... which does not contain a UDP payload.")
-    #     break
-    # elif recv_udp_payload != udp_payload:
-    #     print(
-    #         f"... which contains a UDP payload {recv_udp_payload} that does not match"
-    #     )
-    #     continue
-    # else:
-    #     print("... which contains the right UDP payload!")
-    #     break
+
+# def main():
+#     target_ipv4 = get_ipv4("google.com")
+#     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#
+#     ICMP_PROTO = socket.getprotobyname("icmp")
+#     icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, ICMP_PROTO)
+#     icmp_socket.settimeout(5)
+#
+#     sweep = ProbeRequest.generate_ttl_sweep(target_ipv4, 12)
+#
+#     for probe in sweep:
+#         udp_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, probe.ttl)
+#         time_bs = time.time()
+#         udp_socket.sendto(probe.udp_payload, (probe.ipv4, probe.port))
+#         probe.status = ProbeStatus.DISPATCHED
+#         probe.probe_dispatch_ts = time_bs
+#
+#     while True:
+#         try:
+#             data = icmp_socket.recv(1024)
+#             probe_reply = ProbeReply.from_bytes(data)
+#             print(probe_reply)
+#             print("_______________________")
+#         except socket.timeout:
+#             print("No ICMP packet arrived before timeout. Stopping sweep.")
+#             break
+#         except InvalidProbeReplyException as e:
+#             raise e
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(async_main())
