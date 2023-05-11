f1600a7cbe8a50c9053c8a1a3e5d172eeff58c3f
Levi Borodenko <levi.borodenko@smart-steel-technologies.com>
2023-05-11 15:58:03 +0200

    wip tui

--- pingtracer/core/tracer.py
+++ pingtracer/core/tracer.py
@@ -7,8 +7,6 @@ from pingtracer.core.utils import await_or_cancel_on_event, get_ipv4
 
 @dataclass
 class Tracer:
-    target_ipv4: str
-
     dispatcher: RequestDispatcher = field(default_factory=lambda: RequestDispatcher())
     reply_watcher: ICMPReplyWatcher = field(default_factory=lambda: ICMPReplyWatcher())
     stop: asyncio.Event = field(default_factory=lambda: asyncio.Event())
@@ -17,32 +15,67 @@ class Tracer:
 
     @property
     def hops(self) -> list[RouteHop]:
-        return [hop for hop in self._hops if hop.hop_ipv4 is not None]
+        hops = []
+        for hop in self._hops:
+            if hop.hop_ipv4 is None:
+                continue
+            elif hop.hop_ipv4 == hop.target_ipv4:
+                hops.append(hop)
+                break
+            hops.append(hop)
 
-    async def hop_probing(self, hop: int):
-        route_hop = RouteHop(self.target_ipv4, hop, self._found_all_hops)
+        return hops
+
+    async def hop_probing(self, target_ipv4: str, hop: int):
+        route_hop = RouteHop(target_ipv4, hop, self._found_all_hops)
         self._hops.append(route_hop)
         while not self.stop.is_set():
             await route_hop.measure(self.dispatcher, self.reply_watcher)
 
-    async def trace_route(self, max_hops: int = 32) -> asyncio.Event:
+    async def trace_route(self, target_ipv4: str, max_hops: int = 32) -> asyncio.Event:
         asyncio.create_task(self.reply_watcher.icmp_fetching(self.stop))
         for hop in range(1, max_hops + 1):
             if self._found_all_hops.is_set():
                 break
             asyncio.create_task(
-                await_or_cancel_on_event(self.hop_probing(hop), self.stop)
+                await_or_cancel_on_event(self.hop_probing(target_ipv4, hop), self.stop)
             )
             await asyncio.sleep(0.25)
         return self.stop
+
+    @property
+    def trace_status_table(self):
+        columns = [
+            "Hop",
+            "IPv4",
+            "RTT (mean)",
+            "RTT (current)",
+            "RTT (std)",
+            "pings",
+            "loss (%)",
+        ]
+        rows = []
+        for hop in self.hops:
+            row = (
+                hop.hop,
+                hop.hop_ipv4,
+                hop.rtt.exp_avg,
+                hop.rtt.buffer[-1],
+                hop.rtt.exp_std,
+                hop.n_failed_measurements + hop.n_successful_measurements,
+                hop.n_failed_measurements
+                / (hop.n_failed_measurements + hop.n_successful_measurements),
+            )
+            rows.append(row)
+        return (columns, *rows)
 
 
 async def test_route_tracer():
     target_ipv4 = get_ipv4("facebook.com")
     stop_event = asyncio.Event()
 
-    tracer = Tracer(target_ipv4, stop=stop_event)
-    stop_measurement = await tracer.trace_route()
+    tracer = Tracer(stop=stop_event)
+    stop_measurement = await tracer.trace_route(target_ipv4)
     print("****************************")
     await asyncio.sleep(10)
     stop_measurement.set()
--- pingtracer/tui/app.py
+++ pingtracer/tui/app.py
@@ -1,23 +1,44 @@
 import asyncio
 from textual.app import App, ComposeResult
+from textual.reactive import reactive
 import textual.widgets as widgets
 from pingtracer.core.tracer import Tracer
 from pingtracer.core.utils import get_ipv4
 
 
-tracer = Tracer(get_ipv4("google.com"))
+tracer = Tracer()
 
 
 class PingTracer(App):
+    TITLE = "PingTracer v0"
+    SUB_TITLE = "LOOOL"
+    target_host = reactive("google.com")
+
     def compose(self) -> ComposeResult:
         yield widgets.Header(show_clock=True)
-        yield widgets.Pretty([], id="pretty")
+        yield widgets.Input(placeholder="Trace host")
+        yield widgets.DataTable(
+            id="trace-result",
+        )
 
-    async def on_mount(self):
-        self.tracer_task = asyncio.create_task(tracer.trace_route())
+    def on_mount(self):
+        self.query_one(widgets.DataTable).add_columns(*tracer.trace_status_table[0])
 
-    def on_key(self):
-        self.query_one("#pretty").update(tracer.hops)
+    async def on_input_submitted(self):
+        self.query_one("Input", widgets.Input).disabled = True
+        target_ipv4 = get_ipv4(self.target_host)
+        self.sub_title = f"Tracing {target_ipv4}"
+        self.tracer_task = asyncio.create_task(tracer.trace_route(target_ipv4))
+
+    async def on_key(self):
+        table = self.query_one(widgets.DataTable)
+        table = table.clear()
+        table.add_rows(tracer.trace_status_table[1:])
+        if tracer._found_all_hops.is_set():
+            self.query_one("#trace-result", widgets.DataTable).styles.border = (
+                "heavy",
+                "green",
+            )
 
 
 if __name__ == "__main__":
